2c98bb00047dc3ef1d7326cadaff1c191b6b416d
skontan <46364061+skontan@users.noreply.github.com>
2019-09-17 17:36:15 +0200

    Created DOctorNight.js and MafiaBoss.js

---
+++ src/components/DoctorNight.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+
+class DoctorNight extends Component {
+  state = {
+    language: "Swe",
+    text: [
+      { lang: "Swe", content: "Vem vill du rädda?" },
+      { lang: "Eng", content: "Who do you want to save?" },
+      { lang: "Tung", content: "Shokiraba shando lababa?" }
+    ]
+  };
+
+  render() {
+    return (
+      <Grid
+        container
+        direction="column"
+        alignItems="center"
+        justify="center"
+        spacing={4}
+        style={{ minHeight: "98vh", textAlign: "center" }}
+      >
+        <Grid item>
+          <Typography variant="h3">
+            {
+              this.state.text.find(text => text.lang === this.state.language)
+                .content
+            }
+          </Typography>
+        </Grid>
+
+        {this.props.players.map(player => (
+          <Grid item>
+            <Button variant="contained" size="large" color="primary">
+              {player}
+            </Button>
+          </Grid>
+        ))}
+      </Grid>
+    );
+  }
+}
+
+export default DoctorNight;
---
+++ src/components/MafiaBoss.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+
+class NormalMafia extends Component {
+  state = {};
+
+  render() {
+    return (
+      <Grid
+        container
+        direction="column"
+        alignItems="center"
+        justify="center"
+        spacing={4}
+        style={{ minHeight: "98vh", textAlign: "center" }}
+      >
+        <Grid item>
+          <Typography variant="h3">Who do you want to kill?</Typography>
+        </Grid>
+
+        {this.props.players.map(player => (
+          <Grid item>
+            <Button variant="contained" size="large" color="secondary">
+              {player}
+            </Button>
+          </Grid>
+        ))}
+      </Grid>
+    );
+  }
+}
+
+export default NormalMafia;
